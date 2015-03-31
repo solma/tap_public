112ad58c92f6ba70582f82c0826b02154d4e518c
solma <solma@motorola.com>
2015-03-31 14:06:44 -0700

    #23 add unit test template and tests for PowerTransform and Scale module, and some minor codes updates

---
+++ tap-engines/test/tap.engine/TapEngineTestSpec.scala
@@ -0,0 +1,34 @@
+package tap.engine
+
+import org.apache.spark.{SparkConf, SparkContext}
+import org.scalatest.{Suite, BeforeAndAfterAll}
+
+trait TapEngineTestSpec extends BeforeAndAfterAll {
+  this: Suite =>
+
+  private val master = "local[2]"
+  private val appName = this.getClass.getSimpleName
+
+  private var _sc: SparkContext = _
+
+  def sc = _sc
+
+  val conf: SparkConf = new SparkConf()
+    .setMaster(master)
+    .setAppName(appName)
+
+  override def beforeAll(): Unit = {
+    super.beforeAll()
+    TapEngineTestUtil.silenceSpark()
+    _sc = new SparkContext(conf)
+  }
+
+  override def afterAll(): Unit = {
+    if (_sc != null) {
+      _sc.stop()
+      _sc = null
+    }
+
+    super.afterAll()
+  }
+}
